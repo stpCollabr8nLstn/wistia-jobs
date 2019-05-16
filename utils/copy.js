6dd8fdbcd429299291eb3c18f398d45a476cf8d3
stpCollabr8nLstn <amrios2817@gmail.com>
2019-05-15 22:44:59 -0400

    feat(copy): add copy to utils
    creates a place to make quick copy changes without wading through components

---
+++ utils/copy.js
@@ -0,0 +1,47 @@
+export const jobs = {
+  join: {
+    heading: "Join a team that celebrates each other",
+    body:
+      "We’re on the lookout for motivated, curious, and creative people who are super excited about transforming the way businesses use video to communicate. Let’s create something spectacular together."
+  },
+  beYourself: {
+    heading: "Go on, be yourself",
+    body:
+      "Whether you’re a designer, engineer, or sales rep, we know that what you do doesn’t define you. That’s why we foster an environment where everyone is encouraged to bring their unique personalities and skills to the office every single day. Want to plan a Día de Muertos celebration for the team, join in our weekly Board Game Night (score!), or let your oratory skills shine in our annual Storytelling Night? We’re for it! In everything we do—empathy and connection rule."
+  },
+  location: {
+    heading: "Based in Cambridge, MA",
+    body:
+      "With flexible workstations, plenty of natural light, and a wall of snackity snacks—we love our homey Cambridge office. Tour our office"
+  },
+  values: {
+    heading: "Rooted in values",
+    body:
+      "Our values help everyone at Wistia make decisions that help us build a happy, lasting company focused on product leadership. See our values"
+  },
+  human: {
+    heading: "Committed to being human",
+    body:
+      "We’re a team of hard-working, high-fiving, friendly humans (and one hardly-working dog) ready to take on the video world. Meet the team"
+  },
+  pushing: {
+    heading: "Pushing ourselves–and each other–to innovate",
+    body:
+      "With team-wide hackathons, monthly demos, and biweekly show-and-tells, we're all about encouraging experimentation, then sharing the results with each other. (P.S. - Highlighting what didn't go so well–and what was learned in the process–is highly encouraged!)"
+  },
+  diversity: {
+    heading: "Promoting a culture of diversity and inclusion",
+    body:
+      "At Wistia, we work hard to bring together people from a wide variety of backgrounds and identities, all with the goal of building more inclusive, more useful, and more impactful products and experiences. If you’re invested in being part of a team where new ideas and different perspectives are celebrated, you’ve come to the right place."
+  },
+  fun: {
+    heading: '"Fun" is our middle name (if we had one, that is)',
+    body:
+      "From company retreats to happy hours and movie nights, we know how to take breaks (both planned and unplanned) inside and outside the office. Water skiing and board games not your thing? Not to worry–we work hard to make company events inclusive for our entire team. And who knows? You may end up starring in a video in the process."
+  },
+  openings: {
+    heading: "Current openings",
+    body:
+      "We're currently looking to fill the following positions here at our Cambridge, MA headquarters. We're growing fast, so circle back often!"
+  }
+};
