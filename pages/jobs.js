3a610f7655b0cd7ae51d37a175b73fc909b05194
stpCollabr8nLstn <amrios2817@gmail.com>
2019-05-18 22:12:02 -0400

    feat(jobs): add new IconInfo to jobs

--- pages/jobs.js
+++ pages/jobs.js
@@ -1,11 +1,13 @@
 import BeYourself from "../components/content/be-yourself";
 import Join from "../components/content/join";
+import IconInfo from "../components/content/icon-info";
 
 function Jobs() {
   return (
     <div>
       <Join />
       <BeYourself />
+      <IconInfo />
     </div>
   );
 }
