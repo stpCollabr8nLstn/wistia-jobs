62ee7a990422c14fea9ca9122b777c8c6e3446d8
stpCollabr8nLstn <amrios2817@gmail.com>
2019-05-18 22:09:12 -0400

    style(body-text): add modifier for line-height and text-align

--- components/blocks/body-text.js
+++ components/blocks/body-text.js
@@ -1,6 +1,6 @@
 import COLORS from "../../utils/colors";
 
-const BodyText = ({ children }) => (
+const BodyText = ({ children, align, lineHeight }) => (
   <div>
     <div className="BodyText">{children}</div>
     <style jsx>
@@ -9,8 +9,9 @@ const BodyText = ({ children }) => (
           color: ${COLORS.GRAY_5};
           font-size: 21px;
           font-weight: 400;
-          line-height: 1.5;
+          line-height: ${lineHeight || 1.5};
           margin: 25px auto 30px;
+          text-align: ${align || "left"};
         }
       `}
     </style>
